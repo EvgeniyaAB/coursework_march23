04af2fc78c4e8135d92e02d8a75e5c00374d2b2c
EvgeniyaAB <123991893+EvgeniyaAB@users.noreply.github.com>
2023-03-31 23:16:23 +0300

    Update vk_photo.py

--- vk_photo.py
+++ vk_photo.py
@@ -3,7 +3,6 @@ import requests
 import time
 from tqdm import tqdm
 
-
 class VKontakte:
     def __init__(self, token, id):
         self.vk_url = 'https://api.vk.com/method/'
@@ -13,8 +12,6 @@ class VKontakte:
             'access_token': token,
             'owner_id': id
         }
-
-
     def get_photos(self, count=5, offset=0):
         photos_vk_url = self.vk_url + 'photos.get'
         photos_params = {
@@ -24,10 +21,12 @@ class VKontakte:
             'offset': offset,
             'count': count
         }
-        response = requests.get(url=photos_vk_url, params={**self.params, **photos_params})
-        reg = response.json()
-        all_photos = reg['response']['items']
-
+        try:
+            response = requests.get(url=photos_vk_url, params={**self.params, **photos_params})
+            reg = response.json()
+            all_photos = reg['response']['items']
+        except:
+            print('!! Error')
 
         photos_json =[]
         temporary_list = []
@@ -58,20 +57,37 @@ class YandexDisk:
             'Content-Type': 'application/json',
             'Authorization': self.token
         }
+    def create_folder(self, folder_name):
+        folder_url = f'{self.yandex_url}v1/disk/resources'
+        params = {"path": folder_name, "overwrite": "true"}
+        headers = self.get_headers()
+        try:
+            folder = requests.put(url=folder_url, headers=headers, params=params)
 
-    def get_upload_link(self, directory_name):
+            if folder.status_code == 201:
+                print(f"Папка {folder_name} успешно создана")
+            elif folder.status_code == 409:
+                print(f"Папка {folder_name} уже существует")
+            return folder_name
+        except:
+            print('!! Error create')
+
+    def get_upload_link(self, folder_name):
         photos_json = vk.get_photos()
         for json in photos_json:
             photo_url = json['url']
-            disk_file_path = f"{directory_name}/{json['file_name']}"
+            disk_file_path = f"{folder_name}/{json['file_name']}"
             upload_url = f'{self.yandex_url}v1/disk/resources/upload'
             headers = self.get_headers()
             params = {"path": disk_file_path, "url": photo_url}
-            response = requests.post(url=upload_url, headers=headers, params=params)
-            for i in tqdm(response.json()):
-                time.sleep(0.2)
-            if response.status_code == 202:
-                print("Загружено успешно")
+            try:
+                response = requests.post(url=upload_url, headers=headers, params=params)
+                for i in tqdm(response.json()):
+                    time.sleep(0.2)
+                if response.status_code == 202:
+                    print("Загружено успешно")
+            except:
+                print('!! Error download')
 
 
 
@@ -79,4 +95,4 @@ if __name__ == '__main__':
     vk = VKontakte(token='TOKEN', id='ID')
     pprint(vk.get_photos())
     ya = YandexDisk(token='TOKEN')
-    ya.get_upload_link(directory_name=input('Введи название папки: '))
+    ya.get_upload_link(ya.create_folder(folder_name=input('Введи название папки: ')))
